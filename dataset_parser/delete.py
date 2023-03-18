dea4a80bf85cc528c76773caeae5a8b020f4c123
Matthias Pijarowski <pijarowski.matthias@gmail.com>
2023-03-18 08:39:32 +0100

    remove tests

--- dataset_parser/delete.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("localhost", 27017, username="mongoadmin", password="secret")
-print(client.list_database_names())
-client.drop_database("instances_val2017_test")
-print(client.list_database_names())
